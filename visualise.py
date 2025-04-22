f3da0850de40134d0a6cc03736c2d6d8fdef7504
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-04-22 21:52:03 +0000

    Tue, Apr 22, 2025, 5:52 PM -04:00

---
+++ visualise.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def show_city_map(G, parking_lots, user_node, destination_node):
+    pos = nx.spring_layout(G, seed=1)
+
+    # Decide color for each node
+    colors = []
+    labels = {}
+
+    for node in G.nodes():
+        if node == user_node:
+            colors.append("blue")
+            labels[node] = "User"
+        elif node == destination_node:
+            colors.append("yellow")
+            labels[node] = "Dest"
+        elif node in parking_lots:
+            lot = parking_lots[node]
+            if lot["parked"] < lot["capacity"]:
+                colors.append("green")
+            else:
+                colors.append("red")
+            labels[node] = lot["name"]
+        else:
+            colors.append("gray")
+
+    # Draw the graph with custom labels
+    nx.draw(G, pos, with_labels=True, labels=labels, node_color=colors, node_size=500)
+
+    plt.title("City Map with Parking Lots")
+    plt.savefig("city_map.png")
+    print("Map saved as 'city_map.png'")
