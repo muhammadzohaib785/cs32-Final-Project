5e83b239466163d8bb35a06aa5763919989528b1
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-04-22 21:39:45 +0000

    Tue, Apr 22, 2025, 5:39 PM -04:00

---
+++ parkingdata.py
@@ -0,0 +1,7 @@
+# This file contains sample parking lot data for the simulation.
+
+parking_lots = {
+    2: {"name": "Lot A", "capacity": 10, "parked": 3},
+    6: {"name": "Lot B", "capacity": 5, "parked": 5},  # full
+    20: {"name": "Lot C", "capacity": 8, "parked": 4}
+}
