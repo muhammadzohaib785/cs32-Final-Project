9c200ac17396c41cfe67183a6fa96c2668f7cedd
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-04-29 20:14:51 +0000

    Tue, Apr 29, 2025, 4:14 PM -04:00

--- parking_data.py
+++ parking_data.py
@@ -3,5 +3,5 @@
 parking_lots = {
     2: {"name": "Lot A", "capacity": 10, "parked": 3},
     6: {"name": "Lot B", "capacity": 5, "parked": 5},  # full
-    8: {"name": "Lot C", "capacity": 8, "parked": 4}
+    20: {"name": "Lot C", "capacity": 8, "parked": 4}
 }
