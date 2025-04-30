a48978a4e19e0b0b410d6727fcddfc3b20b59197
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-04-30 21:44:03 +0000

    Wed, Apr 30, 2025, 5:44 PM -04:00

--- visualize.py
+++ visualize.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-
-#This was made with the help of chatGPT to demonstrate how my code works
+# This was made with the help of ChatGPT to demonstrate how my code works
 def show_city_map(G, parking_lots, user_node, destination_node, path=None):
     pos = nx.kamada_kawai_layout(G, weight='weight')
     colors = []
@@ -11,24 +10,26 @@ def show_city_map(G, parking_lots, user_node, destination_node, path=None):
     for node in G.nodes():
         if node == user_node:
             colors.append("blue")
-            labels[node] = "User"
+            labels[node] = f"User ({node})"
         elif node == destination_node:
             colors.append("yellow")
-            labels[node] = "Dest"
+            labels[node] = f"Dest ({node})"
         elif node in parking_lots:
             lot = parking_lots[node]
             colors.append("green" if lot["parked"] < lot["capacity"] else "red")
-            labels[node] = lot["name"]
+            labels[node] = f"{lot['name']} ({node})"
         else:
             colors.append("gray")
+            labels[node] = str(node)  # Just the node number for regular nodes
 
+    # Draw nodes with colors and labels
     nx.draw(G, pos, node_color=colors, labels=labels, with_labels=True, node_size=500)
 
-    # Draw edges normally
+    # Draw all edges with widths based on weights
     weights = [G[u][v]['weight'] for u, v in G.edges()]
     nx.draw_networkx_edges(G, pos, width=[w for w in weights])
 
-    # Highlight the shortest path
+    # Highlight the shortest path if provided
     if path:
         edge_path = list(zip(path, path[1:]))
         nx.draw_networkx_edges(G, pos, edgelist=edge_path, edge_color="black", width=3)
@@ -36,6 +37,3 @@ def show_city_map(G, parking_lots, user_node, destination_node, path=None):
     plt.title("City Map with Parking Lots and Shortest Path")
     plt.savefig("city_map.png")
     print("Map saved as 'city_map.png'")
-
-
-
