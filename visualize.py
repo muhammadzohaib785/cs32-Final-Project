2c135d9b725258c0ee1a5db8f65b5856ab06bd2c
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-04-30 21:49:46 +0000

    Wed, Apr 30, 2025, 5:49 PM -04:00

--- visualize.py
+++ visualize.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # This was made with the help of ChatGPT to demonstrate how my code works
-def show_city_map(G, parking_lots, user_node, destination_node, path=None):
-    pos = nx.kamada_kawai_layout(G, weight='weight')
+def show_city_map(G, parking_lots, user_node, destination_node, path=None, size=6):
+    pos = {i * size + j: (j, -i) for i in range(size) for j in range(size)}  # grid layout
     colors = []
     labels = {}
 
@@ -20,16 +20,13 @@ def show_city_map(G, parking_lots, user_node, destination_node, path=None):
             labels[node] = f"{lot['name']} ({node})"
         else:
             colors.append("gray")
-            labels[node] = str(node)  # Just the node number for regular nodes
+            labels[node] = str(node)
 
-    # Draw nodes with colors and labels
     nx.draw(G, pos, node_color=colors, labels=labels, with_labels=True, node_size=500)
 
-    # Draw all edges with widths based on weights
     weights = [G[u][v]['weight'] for u, v in G.edges()]
     nx.draw_networkx_edges(G, pos, width=[w for w in weights])
 
-    # Highlight the shortest path if provided
     if path:
         edge_path = list(zip(path, path[1:]))
         nx.draw_networkx_edges(G, pos, edgelist=edge_path, edge_color="black", width=3)
