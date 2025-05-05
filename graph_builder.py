c77f5d5cf845438c233cc278482ff56a16a951b6
muhammadzohaib785 <muhammadzohaibabbas53@gmail.com>
2025-05-05 01:22:33 +0000

    Sun, May 4, 2025, 9:22 PM -04:00

--- graph_builder.py
+++ graph_builder.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import random
 
+
 def build_random_city_graph(size, min_weight=1, max_weight=10):
-    """
-    Builds a size x size grid graph with random edge weights.
-    Each edge weight is an integer between min_weight and max_weight.
-    """
+
+    #Builds a size x size grid graph with random edge weights.
+    #Each edge weight is an integer between min_weight and max_weight.
+    
     G = nx.grid_2d_graph(size, size)
     G = nx.convert_node_labels_to_integers(G)
 
